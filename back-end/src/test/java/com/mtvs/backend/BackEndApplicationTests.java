e6a27237c3e68227c14045d6871e21b263e53986
lsy1307 <lsy1307@inha.edu>
2024-09-03 17:17:35 +0900

    init project

---
+++ back-end/src/test/java/com/mtvs/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtvs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
