91f89c533dbb1be7e74275a1fa9bae2111505606
표 <pyoya1123@gmail.com>
2024-09-10 15:30:08 +0900

    Feat: 친구 기능 구현 & 테스트 코드 구현

---
+++ back-end/src/main/java/com/mtvs/backend/Friend/service/FriendshipService.java
@@ -0,0 +1,14 @@
+package com.mtvs.backend.Friend.service;
+
+import com.mtvs.backend.Friend.domain.Friendship;
+import com.mtvs.backend.user.domain.User;
+
+import java.util.List;
+
+public interface FriendshipService {
+    void sendFriendRequest(Long requesterId, Long receiverId); // 친구 요청
+    void acceptFriendRequest(Long friendshipId); // 친구 수락
+    void rejectFriendRequest(Long friendshipId); // 친구 요청 거절
+    List<User> getFriends(Long userId); // 친구 목록 조회
+    List<Friendship> getAllFriendship(Boolean isAccepted); // isAccepted값에 따른 모든 Friendship 조회 
+}
