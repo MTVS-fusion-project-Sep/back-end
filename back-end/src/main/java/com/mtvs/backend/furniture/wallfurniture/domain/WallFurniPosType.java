d93e265e9e3f63c86ac562d78bed37e14ef23d33
표 <pyoya1123@gmail.com>
2024-09-22 22:50:54 +0900

    Feat: 벽 가구 기능 수정

--- back-end/src/main/java/com/mtvs/backend/furniture/wallfurniture/domain/WallFurniPosType.java
+++ back-end/src/main/java/com/mtvs/backend/furniture/wallfurniture/domain/WallFurniPosType.java
@@ -1,8 +1,21 @@
 package com.mtvs.backend.furniture.wallfurniture.domain;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum WallFurniPosType {
-    NONE,
-    LEFT_TWO,
-    RIGHT_ONE,
-    RIGHT_TWO
+    NONE(0),
+    LEFT_TWO(1),
+    RIGHT_ONE(2),
+    RIGHT_TWO(3);
+
+    private final int value;
+
+    WallFurniPosType(int value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public int getValue() {
+        return value;
+    }
 }
